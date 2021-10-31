0e6a51302cae445f82289670e660877e821bfba4
Manan Jain <mananjain31jan@gmail.com>
2021-10-31 16:37:45 +0530

    :zap: Views and Urls files changed
    added 2 urls : getmovies and "/"

--- home/url.py
+++ home/url.py
@@ -3,5 +3,6 @@ from . import views
 
 
 urlpatterns = [
+    path('getmovie', views.getmovie,name='getmovie'),
     path('', views.index,name='index'),
 ]
--- home/views.py
+++ home/views.py
@@ -1,13 +1,23 @@
 from typing import TYPE_CHECKING
 from django.http.response import JsonResponse
 from django.shortcuts import render
+from django.views.decorators.csrf import csrf_exempt
 import json
 
+@csrf_exempt
 def index(request):
+    return render(request, "index.html")
+
+@csrf_exempt
+def getmovie(request):
     if request.method == 'POST':
         data = json.loads(request.body)
-        movieTitle = data['movies']
-        print(movieTitle)
-        return JsonResponse(movieTitle), render(request,'index.html')
+        movieTitles = data['movies']
+        print(movieTitles)
+        dummydata = {
+            "Title" : "Don69",
+            "Cast" : "ARYAN KHAN",
+            }
+        return JsonResponse({"success": True, "suggestion" : dummydata})
     else:
-        return render(request,'index.html')
+        return JsonResponse({"success" : False, "error" : "Internal Server Error"})
