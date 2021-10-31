dc8abc1fd7edc5457f16f0c4ea77b78923b4ceaf
Aditya Pathak <sumitpathak861@gmail.com>
2021-10-31 16:04:24 +0530

    Backend server Json working

--- home/views.py
+++ home/views.py
@@ -1,8 +1,13 @@
+from typing import TYPE_CHECKING
+from django.http.response import JsonResponse
 from django.shortcuts import render
+import json
 
 def index(request):
-    return render(request,'index.html')
-
-
-
-#Jash Sharma
+    if request.method == 'POST':
+        data = json.loads(request.body)
+        movieTitle = data['movies']
+        print(movieTitle)
+        return JsonResponse(movieTitle), render(request,'index.html')
+    else:
+        return render(request,'index.html')
